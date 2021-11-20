79d07dab7f884deb8a9e4541638c64644eb112ed
HanHyeonJu <ha10155@naver.com>
2021-11-21 01:04:20 +0900

    17일 복습

---
+++ CH02_Variables/src/integerVariables.java
@@ -0,0 +1,17 @@
+
+public class integerVariables {
+
+	public static void main(String[] args) {
+		// 변수 만들기 1. 변수 선언  2. 변수 초기화
+		int x;
+		x=7;
+		int y =10;
+		int z = x + y;
+		
+		System.out.println("헬로우 월드!");
+		System.out.println(x);
+		System.out.println(y);
+		System.out.println("x + y의 값은 : " +z);
+	}
+
+}
